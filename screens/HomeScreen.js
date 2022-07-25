a663b29644242686eca1760a8c18f2a7fae655fb
nitin1612 <nitxxn16@gmail.com>
2022-07-25 23:55:42 +0530

    categoryFilter

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator, FlatList, StatusBar, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ActivityIndicator, StatusBar, ScrollView } from 'react-native';
 import auth from '@react-native-firebase/auth';
-import { Card } from 'react-native-paper'
+import { Card, TextInput } from 'react-native-paper'
 import { FlatGrid } from 'react-native-super-grid';
+
 
 export default function HomeScreen() {
     const user = auth().currentUser;
@@ -10,13 +11,11 @@ export default function HomeScreen() {
     const [loading, setLoading] = useState(true);
     const [dataSource, setDataSource] = useState([]);
     const [offset, setOffset] = useState(1);
-    // useEffect(() => {
-    //     
-
-    //         .then((res) => res.json())
-    //         .then((json) => setData(json))
-
-    // }, []);
+    const [filteredData, setFilteredData] = useState([]);
+    const [search, setSearch] = useState();
+    const [filter, setFilter] = useState([]);
+    const [f,setF] = useState('');
+    
 
 
     useEffect(() => getCData(), []);
@@ -29,6 +28,7 @@ export default function HomeScreen() {
                 setOffset(offset + 1);
                 setData(json.slice((offset - 1) * 3, (offset * 3)));
                 setDataSource([...dataSource, ...data])
+                setFilteredData([...dataSource, ...data])
                 setLoading(false)
             })
 
@@ -49,7 +49,6 @@ export default function HomeScreen() {
                 console.error(error);
             });
     };
-    console.log(dataSource);
     const renderFooter = () => {
         return (
             <View style={styles.footer}>
@@ -65,6 +64,29 @@ export default function HomeScreen() {
             </View>
         );
     };
+    const searchFilter = (text) => {
+        if (text) {
+            const newData = dataSource.filter((item) => {
+                const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1;
+            });
+            setFilteredData(newData);
+            setSearch(text);
+        } else {
+            setFilteredData(dataSource);
+            setSearch(text);
+        }
+    }
+    const selectedFilter = () => {
+        const ingredients = dataSource.map((e) => { return e.ingredients })
+        const ingredientsFinal = new Set([].concat.apply([], ingredients))
+        setFilter([...ingredientsFinal])
+        const finaldata = dataSource.filter(e => e.ingredients.includes(f) ? e : null)
+        console.log(f)
+        setFilteredData([...finaldata])
+        console.log(filteredData)
+    };
 
     const renderItem = ({ item }) => {
         return (
@@ -76,51 +98,84 @@ export default function HomeScreen() {
                 height: 175,
                 backgroundColor: "#FFF8DC",
                 borderRadius: 10,
+                elevation: 10
             }}>
                 <TouchableOpacity>
                     <Image
                         style={{ height: 120, width: "95%", borderRadius: 10 }}
                         source={{ uri: item.image }}
                     />
-
                     <Text style={{ marginTop: 10, fontSize: 18, fontWeight: "600", color: "#212121" }}>{item.title}</Text>
-
                 </TouchableOpacity>
-
             </View>
         );
     }
     return (
         <View style={{ backgroundColor: "#ffffff" }}>
+            <ScrollView>
 
             <StatusBar barStyle="dark-content" hidden={false} backgroundColor="#FFF" translucent={true} />
-            <ScrollView>
-                <Card style={{ padding: 15, marginTop: 70, borderRadius: 20, width: "96%", marginLeft: 7, backgroundColor: "#b27846", backfaceVisibility: "visible" }}>
-                    <Text style={styles.title}>Welcome back ! 👋🏻</Text>
-                    <View style={{ flexDirection: "row", alignItems: "center", left: 150 }}>
-                        <Image source={{ uri: user?.photoURL }} style={styles.image} />
-                        <Text style={styles.text}>{user?.displayName}</Text>
-                    </View>
-                </Card>
-                <View>
-                    <Text style={{ fontSize: 35, marginTop: 10, fontFamily: "Oswald-Regular", padding: 10, color: "black", opacity: 0.8 }}>Available Menu : </Text>
-                    {/* <FlatList
-                data={dataSource}
-                renderItem={renderItem}
-                ListFooterComponent={renderFooter}
-                scrollEnabled={true}
-            /> */}
-                    <FlatGrid
-                        itemDimension={130}
-                        data={dataSource}
-                        style={styles.gridView}
-                        spacing={10}
-                        renderItem={renderItem}
-                        ListFooterComponent={renderFooter}
-                    />
-
+            <Card style={{ padding: 15, marginTop: 70, elevation: 15, borderRadius: 20, width: "96%", marginLeft: 7, backgroundColor: "#b27846", backfaceVisibility: "visible" }}>
+                <Text style={styles.title}>Welcome back ! 👋🏻</Text>
+                <View style={{ flexDirection: "row", alignItems: "center", left: 150 }}>
+                    <Image source={{ uri: user?.photoURL }} style={styles.image} />
+                    <Text style={styles.text}>{user?.displayName}</Text>
                 </View>
-                {/* <Text style={styles.text}>{user?.email}</Text> */}
+            </Card>
+            <View>
+
+                <View style={{ borderWidth: 1, width: "95%", left: 10, padding: 10, marginTop: 20, borderColor: "black", borderRadius: 10, flexDirection: "row" }}>
+                    <Text style={{ position: "relative", fontSize: 25, top: -10, fontFamily: "Oswald-Regular" }}>Trendy Searches !</Text>
+                    <View style={{ marginTop: 40, left: -160, flexDirection: "column" }}>
+                        <TouchableOpacity onPress={() => {setF('Coffee');selectedFilter()}}>
+                            <Text style={styles.filterText}>Coffee</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => {setF('Traditional');selectedFilter()}}>
+                            <Text style={styles.filterText}>Traditional</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ marginTop: 40, left: -120, flexDirection: "column" }}>
+                        <TouchableOpacity onPress={() => {setF("Espresso");selectedFilter()}}>
+                            <Text style={styles.filterText}>Espresso</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => {setF('Chocolate');selectedFilter()}}>
+                            <Text style={styles.filterText}>Chocolate</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ marginTop: 40, left: -80, flexDirection: "column" }}>
+                        <TouchableOpacity onPress={() => {setF('Panela');selectedFilter()}}>
+                            <Text style={styles.filterText}>Panela</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => {setF('Cream');selectedFilter()}}>
+                            <Text style={styles.filterText}>Cream</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {/* <FlatList
+                    data={filteredData}
+                    renderItem={renderItem}
+                    ListFooterComponent={renderFooter}
+                    scrollEnabled={true}
+                /> */}
+                <Text style={{ fontSize: 30, marginTop: 10, fontFamily: "Oswald-Regular", padding: 10, color: "black", opacity: 0.8 }}>Available Menu : </Text>
+                <TextInput
+                    style={{ height: 45, width: "95%", left: 10, borderRadius: 10, elevation: 10 }}
+                    placeholder='Type to search ...'
+                    value={search}
+                    onChangeText={(text) => searchFilter(text)}
+                />
+                <FlatGrid
+                    itemDimension={130}
+                    data={filteredData}
+                    style={styles.gridView}
+                    spacing={10}
+                    renderItem={renderItem}
+                    ListFooterComponent={renderFooter}
+                    scrollEnabled={true}
+                />
+
+            </View>
+            {/* <Text style={styles.text}>{user?.email}</Text> */}
             </ScrollView>
         </View>
     );
@@ -139,6 +194,12 @@ const styles = StyleSheet.create({
         borderRadius: 150,
         marginBottom: 20,
         flexDirection: "column"
+    },
+    filterText: {
+        fontSize: 15,
+        padding: 10,
+        color: "#212121",
+        fontWeight:"600"
     },
     text: {
         color: "#fff",
